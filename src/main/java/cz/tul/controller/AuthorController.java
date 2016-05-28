3d171de45d9868841659830b9d42268daad3d2ad
Kulis27 <fila.kulis@gmail.com>
2016-05-28 16:15:54 +0200

    MVC configuration

---
+++ src/main/java/cz/tul/controller/AuthorController.java
@@ -0,0 +1,16 @@
+package cz.tul.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AuthorController {
+
+    @RequestMapping(path = "/")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello world!";
+    }
+
+}
