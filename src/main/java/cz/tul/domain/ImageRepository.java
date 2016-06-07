98a15ea8dcd7f0e22112736df98dfb22f442519d
Kulis27 <fila.kulis@gmail.com>
2016-06-07 06:39:15 +0200

    REST API

--- src/main/java/cz/tul/domain/ImageRepository.java
+++ src/main/java/cz/tul/domain/ImageRepository.java
@@ -1,6 +1,7 @@
 package cz.tul.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,5 +11,11 @@ import java.util.List;
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     List<Image> findByName(@Param("name") String name);
+
+    @Query("select i from Image i where i.author.name = :name")
+    List<Image> findByAuthorName(@Param("name") String name);
+
+    @Query("select i from Image i, in (i.tags) t where t.name = :name")
+    List <Image> findByTagName(@Param("name") String name);
 
 }
